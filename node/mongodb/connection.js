a11bd3acf5307703951a0f5662462fb0894155e4
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-08-16 10:30:43 +0530

    mongodb

--- node/mongodb/connection.js
+++ node/mongodb/connection.js
@@ -1,14 +1,19 @@
-var MongoClient = require('mongodb').MongoClient;
-var dburl = "mongodb://localhost:27017/mydb";
-MongoClient.connect(dburl, function (error, db)
-{
-    if (error)
+var MongoClient = require("mongodb").MongoClient;
+var dburl = "mongodb://0.0.0.0:27017/mydb";
+
+async function connectToDatabase() {
+    try
     {
-        console.log(error);
-    }    
-  
-    
-        console.log("connection successfull .");
-    
-    // db.close();
-})
+    var db = await MongoClient.connect(dburl);
+    var dbo = db.db("mydb");
+        console.log("Connection successful.")
+        
+    db.close();
+  } 
+  catch (error) {
+    console.log("Connection failed:", error);
+  }
+}
+
+connectToDatabase();
+console.log("Attempting to establish a connection...");
--- node/mongodb/createcollection.js
+++ node/mongodb/createcollection.js
@@ -1,22 +1,36 @@
 var MongoClient = require('mongodb').MongoClient;
 var dburl = "mongodb://localhost:27017/mydb";
-MongoClient.connect(dburl, function (error, db)
+
+async function createcollection()
 {
-    if (error)
+    try
+    {
+        var db = await MongoClient.connect(dburl);
+        console.log("Connection successfully ");
+
+        var dbo = db.db("mydb");
+        // var collection = dbo.collection("teacher");
+        await dbo.createCollection('principal').then(function (error, result)
+        {
+            if (error)
+            {
+                console.log(error);
+                // console.log("error  .");    
+            }
+            else
+            {
+                console.log("collection created  successfully .");    
+                // console.log(result);    
+            }
+            // return 'done';
+        })
+
+    }
+    catch (error)
     {
         console.log(error);
     }
-    var dbo = db.db("mydb");
-    dbo.createCollection("customers", function (err, res)
-    {
-        if (err)
-        {
-            console.log(err);
-        }
-        else
-        {
-            console.log("collection created successfull ");
-            }
+}
 
-    })
-})
+createcollection();
+console.log("Create collection start . ");
