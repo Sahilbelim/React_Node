190c0dc07054e27c1227bca900684347dc003949
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-08-24 09:26:55 +0530

    .

--- node/pug/lesson1.js
+++ node/pug/lesson1.js
@@ -1,14 +1,13 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-app.set('view engine', 'pug');
-app.set('views','./');
-app.get('/lesson1', function (request, responce) {
-    responce.render('lesson1.pug');
+app.set("view engine", "pug");
+app.set("views", "./");
+app.get("/lesson1", function (request, responce) {
+  responce.render("lesson1.pug");
 });
-app.get('/lesson12', function (req, res)
-{
-    res.render('lesson1.2.pug')
-})
+app.get("/lesson12", function (req, res) {
+  res.render("lesson1.2.pug");
+});
 app.get("/lesson13", function (req, res) {
   res.render("lesson1.3.pug");
 });
